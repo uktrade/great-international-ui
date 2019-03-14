4e64000fbd33e9dd0cd12dbda01b0457d5385722
Beth Menzies <beth@incuna.com>
2019-03-14 16:22:23 +0000

    hook up template with the new page type

--- core/views.py
+++ core/views.py
@@ -38,6 +38,12 @@ class LandingPageCMSView(BaseCMSPage):
     slug = cms.GREAT_HOME_INTERNATIONAL_SLUG
 
 
+class CuratedLandingPageCMSView(GetSlugFromKwargsMixin, BaseCMSPage):
+    active_view_name = 'curated-topic-landing'
+    template_name = 'core/curated_topic_landing_page.html'
+    page_type = 'InternationalCuratedTopicLandingPage'
+
+
 class ArticlePageView(
     ArticleSocialLinksMixin, BreadcrumbsMixin,
     GetSlugFromKwargsMixin, BaseCMSPage,
