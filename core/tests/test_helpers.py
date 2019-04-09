083a48867fd35addcf3b5951e053543236f8af12
Kenneth Siu <kenneth.siu@softwire.com>
2019-04-09 13:56:04 +0100

    Fix some tests

--- core/tests/test_helpers.py
+++ core/tests/test_helpers.py
@@ -1,32 +1,24 @@
 import pytest
 
+from django.urls import reverse
+
 from core import helpers
 
 
-@pytest.mark.parametrize('path,expected_prefix', (
-    ('/', 'en-gb'),
+@pytest.mark.parametrize('path,expect_code', (
+    ('/', None),
+    ('?lang=pt', 'pt'),
     ('/?lang=ar', 'ar'),
     ('/industries?lang=es', 'es'),
     ('/industries/?lang=zh-hans', 'zh-hans'),
     ('/industries/aerospace?lang=de', 'de'),
     ('/industries/automotive/?lang=fr', 'fr'),
 ))
-def test_get_language_from_querystring(client, path, expected_prefix):
-    prefix = helpers.get_language_from_querystring(path)
-    assert prefix == expected_prefix
-
-
-@pytest.mark.parametrize('prefixed_url,exp_url', (
-    ('/de/', '/'),
-    ('/ar/', '/'),
-    ('/es/industries/', '/industries/'),
-    ('/zh-hans/industries/', '/industries/'),
-    ('/de/industries/aerospace/', '/industries/aerospace/'),
-    ('/fr/industries/automotive/', '/industries/automotive/'),
-))
-def test_get_untranslated_url(prefixed_url, exp_url):
-    url = helpers.get_untranslated_url(prefixed_url)
-    assert url == exp_url
+def test_get_language_from_querystring(path, expect_code, rf):
+    url = reverse('index')
+    request = rf.get(url + path)
+    language_code = helpers.get_language_from_querystring(request)
+    assert language_code == expect_code
 
 
 unslugify_slugs = [
