63762b7e3c5d99aee22e5363cb2514eb947780c1
Andy Babic <andyjbabic@gmail.com>
2019-04-11 11:19:12 +0100

    Remove need for filter_by_active_language() to handle None values

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -849,6 +849,10 @@ def test_page_url_mismatch_404(
                 'slug': 'slug',
                 'languages': [('en-gb', 'English')],
             },
+            # Needed to prevent errors when responding for a number of page types
+            'localised_child_pages': [],
+            'child_pages': [],
+            'related_pages': [],
         }
     )
 
