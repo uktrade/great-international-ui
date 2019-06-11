68d01a638ae7ac304e9517e82702335b2066ff33
Unknown <jessica.gilbert@softwire.com>
2019-06-11 13:13:30 +0100

    Removed print

--- core/views.py
+++ core/views.py
@@ -190,7 +190,7 @@ def capital_invest_region_page_context_modifier(context, request):
         return len(filtered_list)
 
     page = context['page']
-    print('\n\n\n\n\n page ', page)
+    
     return {
         'num_of_economics_statistics': count_data_with_field(
             page['economics_stats'], 'number'),
