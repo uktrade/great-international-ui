589f9822e64c7ac1dc35d27dec276fba1e36cc1b
Unknown <jessica.gilbert@softwire.com>
2019-07-08 10:42:01 +0100

    Dont need equal to 0 and not

--- core/helpers.py
+++ core/helpers.py
@@ -147,8 +147,7 @@ class ScaleFilter:
         for scale_chosen in self.selected_scales:
             if scale_chosen.min == 0 and scale_chosen.max == 0:
                 if not opportunity['scale_value']:
-                    return True
-                elif float(opportunity['scale_value']) == 0:
+                    print('\n\n\n\n\n\n\n the scale ', opportunity['scale_value'])
                     return True
             elif scale_chosen.max == 'None':
                 if scale_chosen.min <= float(opportunity['scale_value']):
