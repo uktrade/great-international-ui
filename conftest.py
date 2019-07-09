5dda52c2626dfc3f968eea266831d6c68cb2a4d2
Alessandro De Noia <alessandro.denoia@digital.trade.gov.uk>
2019-07-09 10:49:06 +0100

    placate flake8

--- conftest.py
+++ conftest.py
@@ -24,39 +24,6 @@ def default_context():
 
 
 @pytest.fixture
-def retrieve_profile_data():
-    return {
-        'website': 'http://example.com',
-        'description': 'Ecommerce website',
-        'summary': 'this is a short summary',
-        'number': '01234567',
-        'sectors': ['SECURITY'],
-        'logo': 'nice.jpg',
-        'name': 'Great company',
-        'slug': 'great-company',
-        'keywords': 'word1, word2',
-        'employees': '501-1000',
-        'date_of_creation': '2015-03-02',
-        'verified_with_code': True,
-        'twitter_url': 'http://www.twitter.com',
-        'facebook_url': 'http://www.facebook.com',
-        'linkedin_url': 'http://www.linkedin.com',
-        'supplier_case_studies': [],
-        'modified': '2016-11-23T11:21:10.977518Z',
-        'email_full_name': 'Jeremy',
-        'email_address': 'test@example.com',
-        'postal_full_name': 'Jeremy',
-        'address_line_1': '123 Fake Street',
-        'address_line_2': 'Fakeville',
-        'locality': 'London',
-        'postal_code': 'E14 6XK',
-        'po_box': 'abc',
-        'country': 'GB',
-        'mobile_number': '07506043448',
-    }
-
-
-@pytest.fixture
 def search_results(retrieve_profile_data):
     return {
         'hits': {
