eda53401fff5ca985fd4a4288b254a1c265f6a9f
Jess Gilbert <jessica.gilbert@digital.trade.gov.uk>
2019-07-23 11:22:40 +0100

    Updated cta on sector page to go to trade contact form

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -21,6 +21,7 @@
 - CI-342 - Promoted similar projects on `CapitalInvestOpportunityPage` are now randomised
 - CI-344 - `Similar projects` title in `CapitalInvestOpportunityPage` is now hard coded
 - CI-332 - Sass changes for mobile design for `CapitalInvestOpportunityListingPage`
+- CI-372 - Updated trade CTA on sector pages to go to trade contact form
 
 
 ## [2019.07.08](https://github.com/uktrade/great-international-ui/releases/tag/2019.07.08)
--- core/views.py
+++ core/views.py
@@ -159,6 +159,8 @@ def sector_page_context_modifier(context, request):
 
     page = context['page']
 
+    trade_contact_form = "https://www.great.gov.uk/trade/industries/contact/"
+
     if 'related_opportunities' in page:
         random.shuffle(page['related_opportunities'])
         random_opportunities = page['related_opportunities'][0:3]
@@ -171,7 +173,8 @@ def sector_page_context_modifier(context, request):
             page['statistics'], 'number'),
         'section_three_num_of_subsections': count_data_with_field(
             page['section_three_subsections'], 'heading'),
-        'random_opportunities': random_opportunities
+        'random_opportunities': random_opportunities,
+        'trade_contact_form_url': trade_contact_form
         }
 
 
