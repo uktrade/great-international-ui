f2eb2930899e684a62fcbd41d54c3ce38bada60a
eadpearce <e.pearce@tutanota.com>
2019-07-24 17:25:32 +0100

    Move mappings into core/constants

--- core/mixins.py
+++ core/mixins.py
@@ -13,51 +13,16 @@ from directory_constants import cms
 from directory_cms_client.client import cms_api_client
 from directory_cms_client.helpers import handle_cms_response
 
-
-TEMPLATE_MAPPING = {
-    'InternationalHomePage': 'core/landing_page.html',
-    'InternationalTopicLandingPage': 'core/topic_list.html',
-    'InternationalArticleListingPage': 'core/article_list.html',
-    'InternationalArticlePage': 'core/uk_setup_guide/article_detail.html',
-    'InternationalCampaignPage': 'core/campaign.html',
-    'InternationalSectorPage': 'core/sector_page.html',
-    'InternationalSubSectorPage': 'core/sector_page.html',
-    'InternationalCuratedTopicLandingPage': (
-        'core/how_to_do_business_landing_page.html'),
-    'InternationalGuideLandingPage': (
-        'core/uk_setup_guide/guide_landing_page.html'),
-    'InternationalEUExitFormPage': 'euexit/international-contact-form.html',
-    'InternationalEUExitFormSuccessPage': (
-        'euexit/international-contact-form-success.html'),
-    'InternationalCapitalInvestLandingPage': (
-        'core/capital_invest/capital_invest_landing_page.html'),
-    'CapitalInvestRegionPage': (
-        'core/capital_invest/capital_invest_region_page.html'),
-    'CapitalInvestOpportunityPage': (
-        'core/capital_invest/capital_invest_opportunity_page.html'),
-    'CapitalInvestOpportunityListingPage': (
-        'core/capital_invest/capital_invest_opportunity_listing_page.html'),
-    'AboutDitServicesPage': 'core/about_dit/services_page.html',
-    'AboutUkWhyChooseTheUkPage': 'core/about_uk/why_choose_the_uk_page.html',
-    # Invest
-    'InvestInternationalHomePage': 'invest/landing_page.html',
-    'InvestHighPotentialOpportunityDetailPage': (
-        'invest/high_potential_opportunity_detail.html'),
-}
-
-FEATURE_FLAGGED_URLS_MAPPING = {
-    '/international/content/how-to-do-business-with-the-uk/': (
-        'HOW_TO_DO_BUSINESS_ON'),
-}
+from core import constants
 
 
 class NotFoundOnDisabledFeature:
     def dispatch(self, *args, **kwargs):
 
-        if self.request.path not in FEATURE_FLAGGED_URLS_MAPPING:
+        if self.request.path not in constants.FEATURE_FLAGGED_URLS_MAPPING:
             return super().dispatch(*args, **kwargs)
 
-        flag = FEATURE_FLAGGED_URLS_MAPPING.get(self.request.path, None)
+        flag = constants.FEATURE_FLAGGED_URLS_MAPPING.get(self.request.path, None)
         flag_on = settings.FEATURE_FLAGS.get(flag, False)
 
         if not flag_on:
@@ -91,7 +56,7 @@ class CMSPageFromSlugMixin:
 
     @property
     def template_name(self):
-        return TEMPLATE_MAPPING[self.page['page_type']]
+        return constants.TEMPLATE_MAPPING[self.page['page_type']]
 
     def dispatch(self, request, *args, **kwargs):
         """
