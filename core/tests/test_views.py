82c5bc4b05109fca65f776228b7a53ff30796f49
JessGilbert <jessica.gilbert@digital.trade.gov.uk>
2019-08-29 09:28:29 +0100

    More tests

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -2099,3 +2099,105 @@ def test_getting_region_labels_with_coordinates_on_about_uk_page(
     assert response.context_data['south_england'][2]['y'] == '701.0'
     assert response.context_data['south_england'][3]['x'] == '485'
     assert response.context_data['south_england'][3]['y'] == '726.0'
+
+
+@patch('directory_cms_client.client.cms_api_client.lookup_by_path')
+def test_getting_region_labels_with_coordinates_on_about_uk_page_when_null(
+        mock_cms_response, rf
+):
+    page = {
+        'title': 'About UK',
+        'meta': {
+            'languages': [
+                ['en-gb', 'English'],
+            ]
+        },
+        'page_type': 'AboutUkLandingPage',
+    }
+
+    mock_cms_response.return_value = create_response(page)
+
+    request = rf.get('/international/content/about-uk/')
+    request.LANGUAGE_CODE = 'en-gb'
+    response = MultilingualCMSPageFromPathView.as_view()(
+        request, path='/international/content/about-uk/')
+
+    assert response.context_data['scotland'] == []
+
+
+@patch('directory_cms_client.client.cms_api_client.lookup_by_path')
+def test_getting_region_labels_with_coordinates_on_region_listing_page(
+        mock_cms_response, rf
+):
+    page = {
+        'title': 'Regions',
+        'meta': {
+            'languages': [
+                ['en-gb', 'English'],
+            ]
+        },
+        'page_type': 'AboutUkRegionListingPage',
+        'mapped_regions': [
+            {'region': {'meta': {'slug': 'scotland'}, 'title': 'Scotland'}, 'text': 'Lorem ipsum'},
+            {'region': {'meta': {'slug': 'northern-ireland'}, 'title': 'The Northern Ireland'}, 'text': 'Lorem ipsum'},
+            {'region': {'meta': {'slug': 'north-england'}, 'title': 'North England'}, 'text': 'Lorem ipsum'},
+            {'region': {'meta': {'slug': 'wales'}, 'title': 'Wales'}, 'text': 'Lorem ipsum'},
+            {'region': {'meta': {'slug': 'midlands'}, 'title': 'Midlands'}, 'text': 'Lorem ipsum'},
+            {'region': {'meta': {'slug': 'south-england'}, 'title': 'The South of England'}, 'text': 'Lorem ipsum'},
+        ],
+    }
+
+    mock_cms_response.return_value = create_response(page)
+
+    request = rf.get('/international/content/about-uk/')
+    request.LANGUAGE_CODE = 'en-gb'
+    response = MultilingualCMSPageFromPathView.as_view()(
+        request, path='/international/content/about-uk/')
+
+    assert response.context_data['scotland'][0]['x'] == '164'
+    assert response.context_data['scotland'][0]['y'] == '206.0'
+
+    assert response.context_data['north_england'][0]['x'] == '440'
+    assert response.context_data['north_england'][0]['y'] == '415.0'
+    assert response.context_data['north_england'][1]['x'] == '440'
+    assert response.context_data['north_england'][1]['y'] == '440.0'
+
+    assert response.context_data['northern_ireland'][0]['x'] == '195'
+    assert response.context_data['northern_ireland'][0]['y'] == '347.5'
+    assert response.context_data['northern_ireland'][1]['x'] == '195'
+    assert response.context_data['northern_ireland'][1]['y'] == '372.5'
+    assert response.context_data['northern_ireland'][2]['x'] == '195'
+    assert response.context_data['northern_ireland'][2]['y'] == '397.5'
+
+    assert response.context_data['south_england'][0]['x'] == '485'
+    assert response.context_data['south_england'][0]['y'] == '651.0'
+    assert response.context_data['south_england'][1]['x'] == '485'
+    assert response.context_data['south_england'][1]['y'] == '676.0'
+    assert response.context_data['south_england'][2]['x'] == '485'
+    assert response.context_data['south_england'][2]['y'] == '701.0'
+    assert response.context_data['south_england'][3]['x'] == '485'
+    assert response.context_data['south_england'][3]['y'] == '726.0'
+
+
+@patch('directory_cms_client.client.cms_api_client.lookup_by_path')
+def test_getting_region_labels_with_coordinates_on_region_listing_page_when_null(
+        mock_cms_response, rf
+):
+    page = {
+        'title': 'Regions',
+        'meta': {
+            'languages': [
+                ['en-gb', 'English'],
+            ]
+        },
+        'page_type': 'AboutUkRegionListingPage',
+    }
+
+    mock_cms_response.return_value = create_response(page)
+
+    request = rf.get('/international/content/about-uk/')
+    request.LANGUAGE_CODE = 'en-gb'
+    response = MultilingualCMSPageFromPathView.as_view()(
+        request, path='/international/content/about-uk/')
+
+    assert response.context_data['scotland'] == []
