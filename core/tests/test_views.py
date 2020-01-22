4f83f48faee8196263a89717276170fcc5aab6ba
Edie Pearce <edie.pearce@digital.trade.gov.uk>
2020-01-22 16:59:28 +0000

    Update capital invest form

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -1885,9 +1885,9 @@ def capital_invest_contact_form_data(captcha_stub):
         'email_address': 'captain_america@avengers.com',
         'phone_number': '01234 567891',
         'country': 'FR',
-        'city': 'Kentucky',
-        'company_name': 'Guardian of the Galaxy',
         'message': 'foobar',
+        'email_contact_consent': True,
+        'telephone_contact_consent': True,
         'g-recaptcha-response': captcha_stub,
         'terms_agreed': True
     }
@@ -2284,9 +2284,9 @@ def test_capital_invest_contact_serialized_data(mock_save, capital_invest_contac
         'email_address': capital_invest_contact_form_data['email_address'],
         'phone_number': capital_invest_contact_form_data['phone_number'],
         'country': capital_invest_contact_form_data['country'],
-        'city': capital_invest_contact_form_data['city'],
-        'company_name': capital_invest_contact_form_data['company_name'],
-        'message': capital_invest_contact_form_data['message']
+        'message': capital_invest_contact_form_data['message'],
+        'email_contact_consent': capital_invest_contact_form_data['email_contact_consent'],
+        'telephone_contact_consent': capital_invest_contact_form_data['telephone_contact_consent'],
     }
 
 
