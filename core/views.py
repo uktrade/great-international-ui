3ed7a98ec4640e8c30ebd51ba05c3e946d954df8
GabeMaker <gabriel_arafa@hotmail.com>
2020-02-13 14:56:21 +0000

    CI-614 fix for case where no related sector is added to opportunity

--- core/views.py
+++ core/views.py
@@ -354,14 +354,19 @@ def about_uk_region_page_context_modifier(context, request):
 @register_context_modifier('CapitalInvestOpportunityPage')
 def capital_invest_opportunity_page_context_modifier(context, request):
 
-    related_sector_dictionary = context['page']['related_sectors'][0]['related_sector']
-    current_sector_title = related_sector_dictionary['title']
+    related_sectors = context['page']['related_sectors']
 
-    return {
-        'invest_cta_link': urls.international.EXPAND_HOME,
-        'buy_cta_link': urls.international.TRADE_HOME,
-        'current_sector_title': current_sector_title.capitalize(),
-    }
+    if related_sectors:
+        current_sector_title = related_sectors[0]['related_sector']['title']
+
+        return {
+            'invest_cta_link': urls.international.EXPAND_HOME,
+            'buy_cta_link': urls.international.TRADE_HOME,
+            'current_sector_title': current_sector_title,
+            }
+
+    else:
+        return {}
 
 
 class OpportunitySearchView(CountryDisplayMixin, InternationalView):
