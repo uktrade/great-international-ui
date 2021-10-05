6e3d68adeb29b5e316127d014ea392cf8d70a612
Fred Marecesche <fred@froddd.co.uk>
2021-10-05 12:25:29 +0100

    Fix error when blank related sector selected

--- investment_atlas/tests/test_helpers.py
+++ investment_atlas/tests/test_helpers.py
@@ -13,6 +13,22 @@ def test_get_sectors_label():
     assert helpers.get_sectors_label(page) == 'Housing (Green housing, Urban, Renting)'
 
 
+def test_get_sectors_label_undefined_sectors():
+    page = {}
+
+    assert helpers.get_sectors_label(page) == ''
+
+
+def test_get_sectors_label_undefined_sub_sectors():
+    page = {  # NOQA
+        'related_sectors': [
+            {'related_sector': {'title': 'Housing'}}
+        ]
+    }
+
+    assert helpers.get_sectors_label(page) == 'Housing'
+
+
 def test_get_sectors_label_no_sector():
     page = {  # NOQA
         'related_sectors': [],
@@ -29,6 +45,27 @@ def test_get_sectors_label_no_subsectors():
             {'related_sector': {'title': 'Aerospace'}}
         ],
         'sub_sectors': []
+    }
+
+    assert helpers.get_sectors_label(page) == 'Housing'
+
+
+def test_get_sectors_label_blank_sector():
+    page = {  # NOQA
+        'related_sectors': [
+            {'related_sector': []}
+        ]
+    }
+
+    assert helpers.get_sectors_label(page) == ''
+
+
+def test_get_sectors_label_blank_sub_sector():
+    page = {  # NOQA
+        'related_sectors': [
+            {'related_sector': {'title': 'Housing'}},
+        ],
+        'sub_sectors': ['']
     }
 
     assert helpers.get_sectors_label(page) == 'Housing'
