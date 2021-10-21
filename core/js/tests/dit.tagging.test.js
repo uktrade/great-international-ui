08f04462fc37cbcc025d62743eeb2177f99c1779
Fred Marecesche <fred@froddd.co.uk>
2021-10-21 12:45:37 +0100

    Add test for debug mode

--- core/js/tests/dit.tagging.test.js
+++ core/js/tests/dit.tagging.test.js
@@ -36,7 +36,7 @@ describe('links event tracking', () => {
     });
 
     it('pushes to data layer on clicking a link', () => {
-        // Original dit.tagging.js use the `mouseup` event here, probably hoping to include clicks
+        // Original dit.tagging.js uses the `mouseup` event here, probably hoping to include clicks
         // using the middle mouse button.
         // This isn't very useful, for several reasons, hence we move to using `click` instead:
         // - Tap events on touch devices do not trigger a `mouseup` event
@@ -251,6 +251,34 @@ describe('form events tracking', () => {
             'element': '',
             'value': '/foo-bar',
             'formData': 'foo=1&bar=baz'
+        });
+    });
+})
+
+describe('in debug mode', () => {
+    beforeEach(async () => {
+        document.body.innerHTML = `
+            <a href="#link-basic">Basic link</a>
+        `;
+        window.dataLayer = [];
+        dit.tagging.base.init(true);
+        window.document.dispatchEvent(new Event("DOMContentLoaded", {
+            bubbles: true,
+            cancelable: true
+        }));
+        window.console.log = jest.fn();
+    });
+
+    it('logs to console on clicking a link', () => {
+        document.querySelector('[href="#link-basic"]').click();
+
+        expect(window.console.log).toHaveBeenCalledWith({
+            'event': 'gaEvent',
+            'action': 'clickLink',
+            'type': 'PageLink',
+            'element': '',
+            'value': 'Basic link',
+            'destination': '#link-basic'
         });
     });
 })
