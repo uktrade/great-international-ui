4cde6ecb16557aec7ce82d0362fee3d5a6f392d6
davidu1975 <david.urquhart@digital.trade.gov.uk>
2023-07-20 15:55:07 +0100

    wip

--- find_a_supplier/templatetags/industry_tags.py
+++ find_a_supplier/templatetags/industry_tags.py
@@ -30,7 +30,7 @@ def search_url(sector_value=None, term=None):
 
 
 @register.filter
-def add_export_elements_classes(value, color):
+def add_export_elements_classes(value, style):
     soup = BeautifulSoup(value, 'html.parser')
     mapping = [
         ('h1', 'govuk-heading-xl great-text-white'),
@@ -41,7 +41,7 @@ def add_export_elements_classes(value, color):
         ('h6', 'govuk-heading-s'),
         ('ul', 'list list-bullet'),
         ('ol', 'list list-number'),
-        ('p', 'govuk-body-white' if color == 'W' else 'govuk-body'),
+        ('p', style),
         ('a', 'link'),
         ('blockquote', 'quote'),
         ('strong', 'great-bold-small'),
@@ -50,14 +50,6 @@ def add_export_elements_classes(value, color):
         for element in soup.findAll(tag_name):
             element.attrs['class'] = class_name
     return mark_safe(str(soup))
-
-
-@register.filter
-def convert_headings_to(value, heading):
-    soup = BeautifulSoup(value, 'html.parser')
-    for element in soup.findAll(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']):
-        element.name = heading
-    return str(soup)
 
 
 @register.inclusion_tag('find_a_supplier/banner.html')
