891c63d0b0eb617d78f9d402061e1549ef7ed911
davidu1975 <david.urquhart@digital.trade.gov.uk>
2024-07-18 10:28:18 +0100

    refactor canonical url template tag and add new test

--- core/templatetags/canonical_url_tags.py
+++ core/templatetags/canonical_url_tags.py
@@ -4,8 +4,14 @@ from core.utils import derive_canonical_url
 
 register = template.Library()
 
+PATH_POS = 1
+PATH_STR = '/international/investment-support-directory/'
+
 
 @register.simple_tag(takes_context=True)
 def get_canonical_url(context):
+    breakpoint()
     request = context['request']
+    if request.path.startswith(PATH_STR):
+        request.path = request.path.partition(PATH_STR)[PATH_POS]
     return derive_canonical_url(request)
